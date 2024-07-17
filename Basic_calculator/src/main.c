1132ed882e8a4240f75c31b69cff2078f493a9da
Anush Rathod <anush.rathore382@gmail.com>
2024-07-17 17:17:44 -0400

    calc v2 with if statements

--- Basic_calculator/src/main.c
+++ Basic_calculator/src/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+//Version 1
+/*
 int main() {
 
     double num1, num2;
@@ -13,4 +15,35 @@ int main() {
     return 0;
 
 
+}
+*/
+
+/*------------------------------------------------*/
+
+//Version 2
+
+int main (){
+    double num1, num2;
+    char op;
+
+    printf("Enter the first Number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second Number: ");
+    scanf("%lf", &num2);
+
+    printf("Enter Operator: ");
+    scanf(" %c", &op);
+
+    if (op == '+') {
+        printf("%f\n", num1 + num2);
+    }else if (op == '-'){
+        printf("%f\n", num1 - num2);
+    }else if (op == '*'){
+        printf("%f\n", num1 * num2);
+    }else if (op == '/'){
+        printf("%f\n", num1 / num2);
+    }else {
+        printf("invalid");
+    }
 }
